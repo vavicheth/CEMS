46fc4dd502ea4c0bb990f0365bc6ce74ba3ccd03
vavicheth <vicheth_it@yahoo.com>
2020-07-03 09:27:20 +0700

    Add address

---
+++ database/migrations/2020_07_02_101620_create_provinces_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvincesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->id();
+            $table->integer('code');
+            $table->string('name');
+            $table->string('name_kh');
+            $table->string('type');
+            $table->string('type_kh');
+            $table->string('reference')->nullable();
+            $table->string('official_note')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provinces');
+    }
+}
